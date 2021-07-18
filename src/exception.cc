2975ad97a239c39c6408b979da93963e608bbc16
David Chisnall <davidchisnall@users.noreply.github.com>
2021-07-18 21:17:47 +0100

    David Chisnall is not incorporated

--- src/exception.cc
+++ src/exception.cc
@@ -1,6 +1,6 @@
 /* 
  * Copyright 2010-2011 PathScale, Inc. All rights reserved.
- * Copyright 2021 David Chisnall, Inc. All rights reserved.
+ * Copyright 2021 David Chisnall. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
