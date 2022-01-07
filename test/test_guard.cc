fd484be8d1e94a1fcf6bc5c67e5c07b65ada19b6
David Chisnall <davidchisnall@users.noreply.github.com>
2022-01-07 16:02:13 +0000

    Atomics cleanup (#11)
    We need to test exception specifiers but they're gone in C++17 so
    compile the tests with an older version of the standard.
    
    Rewrite the guard logic to be more idiomatic C++ and more
    comprehensible and make sure that atomics are used where necessary.

--- test/test_guard.cc
+++ test/test_guard.cc
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
 #include "test.h"
 
 static int static_count;
@@ -20,10 +22,44 @@ int init_static(void)
 	return s.i;
 }
 
+thread_local bool mainThread = false;
+
+void *init_static_race(void*);
+
+static int instances = 0; 
+struct static_slow_struct
+{
+	int field;
+	static_slow_struct()
+	{
+		if (mainThread)
+		{
+			pthread_t thr;
+			// See if another thread can make progress while we hold the lock.
+			// This will happen if we get the lock and init bytes the wrong way
+			// around.
+			pthread_create(&thr, nullptr, init_static_race, nullptr);
+			sleep(1);
+		}
+		field = 1;
+		instances++;
+	};
+};
+
+void *init_static_race(void*)
+{
+	static static_slow_struct s;
+	TEST(s.field == 1, "Field correctly initialised");
+	return nullptr;
+}
+
 void test_guards(void)
 {
 	init_static();
 	int i = init_static();
 	TEST(i == 12, "Static initialized");
 	TEST(static_count == 2, "Each static only initialized once");
+	mainThread = true;
+	init_static_race(nullptr);
+	TEST(instances == 1, "Two threads both tried to initialise a static");
 }
